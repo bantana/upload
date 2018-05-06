9b8db3ddf655a4da585bf1678a9c4d8a5e6121e2
bantana <bantana@gmail.com>
2018-05-06 17:21:32 +0800

    static file server test

--- labs/simple/main.go
+++ labs/simple/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 	// "io"
 	// "io/ioutil"
 	// "log"
@@ -10,7 +11,29 @@ import (
 	// "mime/multipart"
 )
 
-func main() {
-	// fmt.Println("starting server")
-	log.Println("starting server...")
+//  fileServer define Upload file director and Port
+type fileServer struct {
+	Dir  string
+	Port string
+	URL  string
 }
+
+func main() {
+	mux := http.NewServeMux()
+	myserver := &fileServer{"./public", ":3001", "/file"}
+	// fmt.Println("starting server")
+	log.Println("something run tips here!")
+	log.Printf("tips: staring server at port: %s use local dir: %s,", myserver.Port, myserver.Dir)
+	log.Printf("tips: access path: %s", myserver.URL)
+
+	// mux.Handle("/", welcomeHandler())
+	mux.Handle("/file/", http.StripPrefix("/file/", http.FileServer(http.Dir(myserver.Dir))))
+
+	log.Fatal(http.ListenAndServe(myserver.Port, mux))
+}
+
+// func welcomeHandler() http.Handler {
+// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		w.Write([]byte("Welome"))
+// 	})
+// }
